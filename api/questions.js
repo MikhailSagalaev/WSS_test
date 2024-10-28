df9614e063887042cb823eaec932c6c8f9d689c3
Mixa <101730295+MikhailSagalaev@users.noreply.github.com>
2024-10-28 22:24:55 +0300

    tilda

--- api/questions.js
+++ api/questions.js
@@ -2,6 +2,21 @@
 const fetch = require('node-fetch');
 
 module.exports = async (req, res) => {
+    // Настройка CORS
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    );
+
+    // Обработка OPTIONS запросов
+    if (req.method === 'OPTIONS') {
+        res.status(200).end();
+        return;
+    }
+
     console.log("Получен запрос к /api/questions");
 
     if (req.method !== 'GET') {
